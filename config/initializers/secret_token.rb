4f19161f51b21fd3a5a0f5322bd3ae5357fac84a
Jeremy Neander <subjectego@gmail.com>
2012-09-28 10:02:10 -0500

    Create Rails project Dojo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dojo::Application.config.secret_token = '72f4d89dc474be90ff9b6c9b7dc6f743dc0472498d738f84cada415e270f6789fd76dd7369a1188ed09ade934ee38c37fbd05581460a498180703629523a1ba3'
