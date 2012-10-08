16f2cb0e8c87b440bd15b895c7b7e93e102785da
Jeremy Neander <subjectego@gmail.com>
2012-10-08 11:40:47 -0500

    Swap records attribute reference to method call
    The @records attribute could potentially be used before being
    initialized in the #records method call. Use this method call in place
    of all calls to @records, except within the #destroy_all method.

--- lib/dojo/kata_repository.rb
+++ lib/dojo/kata_repository.rb
@@ -12,7 +12,7 @@ module Dojo
       clone = kata.dup
       clone.id = clone.id || records.size + 1
       clone.last_updated = DateTime.now
-      @records[clone.id] = clone
+      records[clone.id] = clone
     end
 
     def find(id)
@@ -24,7 +24,7 @@ module Dojo
     end
 
     def sort
-      @records.values.sort {|a,b| b.last_updated <=> a.last_updated}
+      records.values.sort {|a,b| b.last_updated <=> a.last_updated}
     end
 
     def destroy_all
