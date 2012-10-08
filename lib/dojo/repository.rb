efaeddb06fca7544cf371f78eaeaa615db436142
Jeremy Neander <subjectego@gmail.com>
2012-10-08 14:47:14 -0500

    Use FeedbackRepository singleton in Repository

--- lib/dojo/repository.rb
+++ lib/dojo/repository.rb
@@ -1,10 +1,15 @@
 require 'dojo/kata_repository'
+require 'dojo/feedback_repository'
 
 module Dojo
   class Repository
 
     def self.kata
       @kata ||= Dojo::KataRepository.new
+    end
+
+    def self.feedback
+      @feedback ||= Dojo::FeedbackRepository.new
     end
 
   end
--- spec/dojo/repository_spec.rb
+++ spec/dojo/repository_spec.rb
@@ -2,10 +2,16 @@ require 'dojo/repository'
 
 describe Dojo::Repository do
   
-  it "#kata refers to a KataRepository singleton" do
+  it "::kata refers to a KataRepository singleton" do
     repo = Dojo::Repository.kata
     repo.should be_a Dojo::KataRepository
     Dojo::Repository.kata.should equal repo
+  end
+
+  it "::feedback refers to a FeedbackRepository singleton" do
+    repo = Dojo::Repository.feedback
+    repo.should be_a Dojo::FeedbackRepository
+    Dojo::Repository.feedback.should equal repo
   end
 
 end
