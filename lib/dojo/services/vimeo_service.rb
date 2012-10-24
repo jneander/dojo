0f313c7b70e3cd00558b22c430a03c35de935536
Jeremy Neander <subjectego@gmail.com>
2012-10-23 23:53:19 -0500

    Create service for handling Vimeo content
    A VimeoService static class should handle requests for Vimeo content.
    The :embed static method should receive a URI object and return a
    VimeoVideo object with the following attributes:
     - title "the Vimeo title for the video"
     - width "the horizontal resolution of the video"
     - height "the vertical resolution of the video"
     - video_id "the Vimeo id corresponding with the video"

---
+++ lib/dojo/services/vimeo_service.rb
@@ -0,0 +1,33 @@
+require 'net/http'
+require 'xmlsimple'
+
+module Dojo
+  module Media
+    class VimeoService
+
+      def self.embed(uri)
+        api_call = oembed(uri)
+        # should be smart enough to fail politely on 'no internet'
+        data = XmlSimple.xml_in(Net::HTTP.get_response(api_call).body)
+
+        video = VimeoVideo.new
+        video.title = data["title"].first
+        video.height = data["height"].first.to_i
+        video.width = data["width"].first.to_i
+        video.id = data["video_id"].first.to_i
+
+        return video
+      end
+
+      def self.oembed(uri)
+        media_path = "https%3a//vimeo.com#{uri.path}"
+        URI.parse("http://vimeo.com/api/oembed.xml?url=#{media_path}")
+      end
+
+    end
+
+    class VimeoVideo
+      attr_accessor :title, :height, :width, :id
+    end
+  end
+end
---
+++ spec/dojo/services/vimeo_service_spec.rb
@@ -0,0 +1,60 @@
+require 'dojo/services/vimeo_service'
+require 'URI'
+require 'net/http'
+
+describe Dojo::Media::VimeoService do
+
+  let(:service) { Dojo::Media::VimeoService }
+  let(:uri) { URI.parse('https://vimeo.com/50459431') }
+  let(:body) { '<?xml version="1.0" encoding="UTF-8"?>\n
+      <oembed>
+        <title>Bowling Game Kata in Clojure</title>
+        <height>720</height>
+        <width>1152</width>
+        <video_id>50459341</video_id>
+      </oembed>\n' }
+  
+  before(:each) do
+    ok_response = mock( Net::HTTPOK )
+    ok_response.stub!(:body).and_return(body)
+    Net::HTTP.stub!(:get_response).and_return(ok_response)
+  end
+
+  it ":oembed returns the Vimeo API call URI" do
+    oembed = "http://vimeo.com/api/oembed.xml?url=https%3a//vimeo.com/50459431"
+    actual = Dojo::Media::VimeoService.oembed(uri)
+    actual.should be_instance_of(URI::HTTP)
+    actual.to_s.should == oembed
+  end
+
+  it ":embed accepts a URI argument" do
+    service.embed(uri)
+  end
+
+  it ":embed returns an instance of VimeoVideo" do
+    service.embed(uri).should be_instance_of( Dojo::Media::VimeoVideo )
+  end
+
+  it ":embed returns a VimeoVideo formed from the given URI" do
+    video = service.embed(uri)
+    video.title.should == "Bowling Game Kata in Clojure"
+    video.width.should == 1152
+    video.height.should == 720
+    video.id.should == 50459341
+  end
+
+  it ":embed has default behavior when network is unavailable"
+
+end
+
+describe Dojo::Media::VimeoVideo do
+
+  it "has accessor attributes" do
+    video = Dojo::Media::VimeoVideo.new
+    [:title, :height, :width, :id].each do |attr|
+      video.should respond_to(attr)
+      video.should respond_to((attr.to_s + "=").to_sym)
+    end
+  end
+
+end
