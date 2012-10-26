d85d0cc4aca43a225e6ddf854940dfa466f1afec
Jeremy Neander <subjectego@gmail.com>
2012-10-26 18:33:16 -0500

    Add validations to Feedback creation

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,11 +1,29 @@
+require 'dojo/models/feedback'
 require 'dojo/repository'
+require 'dojo/validation/feedback_validator'
 
 class FeedbackController < ApplicationController
 
   def create
-    @feedback = Dojo::Repository.feedback.new(params)
-    @feedback = Dojo::Repository.feedback.save(@feedback)
-    redirect_to kata_path(@feedback.kata_id)
+    kata_id = params[:kata_id]
+
+    if Dojo::FeedbackValidator.valid?( params )
+      create_feedback( params )
+    else
+      fields = Dojo::Feedback.attributes
+      flash[:form_values] = symbolize_keys( params ).
+        select { |k,v| fields.include? k }
+      flash[:errors] = Dojo::FeedbackValidator.errors( params )
+    end
+
+    redirect_to kata_path( kata_id )
+  end
+
+  private
+
+  def create_feedback( params )
+    repo = Dojo::Repository.feedback
+    repo.save( repo.new( params ))
   end
 
 end
--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -1,3 +1,4 @@
+require 'dojo/validation/feedback_validator'
 require 'spec_helper'
 
 describe FeedbackController do
@@ -10,20 +11,47 @@ describe FeedbackController do
 
   describe "POST 'create'" do
     
-    it "creates a Feedback instance" do
+    it "with valid parameters creates a Feedback instance" do
       lambda { post :create, attr }.
         should change(repo.records, :size).by(1)
       last_record(repo).author.should == "Jeremy"
     end
 
-    it "assigns the Feedback instance" do
-      post :create, attr
-      assigns(:feedback).should == last_record(repo)
+    context "with invalid parameters" do
+
+      let(:attr) {{ author: "", kata_id: 1, message: "" }}
+      let(:errors) {{ author: "author error" }}
+
+      before(:each) do
+        Dojo::FeedbackValidator.stub!( :errors ).and_return( errors )
+      end
+
+      it "does not create a Feedback instance" do
+        lambda { post :create, attr }.
+          should_not change( repo.records, :size )
+      end
+
+      it "redirects to the kata show page" do
+        post :create, attr
+        response.should redirect_to( kata_path( 1 ))
+      end
+
+      it "sends a flash for form_values through the redirect" do
+        post :create, attr
+        expected = Hash[attr.map { |k,v| [k, v.to_s] }]
+        flash[:form_values].should == expected
+      end
+
+      it "sends a flash for errors through the redirect" do
+        post :create, attr
+        flash[:errors].should == errors
+      end
+
     end
 
     it "redirects to the kata show page" do
       post :create, attr
-      response.should redirect_to(kata_path(assigns(:feedback).kata_id))
+      response.should redirect_to( kata_path( 1 ))
     end
 
   end
