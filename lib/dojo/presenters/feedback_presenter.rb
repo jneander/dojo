2052ab958fcc46ba15a95badedf9818e0ad77305
Jeremy Neander <subjectego@gmail.com>
2012-10-30 00:21:49 -0500

    Create presenter for Feedback

---
+++ lib/dojo/presenters/feedback_presenter.rb
@@ -0,0 +1,26 @@
+require 'dojo/repository'
+
+module Dojo
+
+  class FeedbackPresenter
+
+    def initialize( feedback )
+      @feedback = feedback
+      @user = Dojo::Repository.user.find( @feedback.user )
+    end
+
+    def message
+      @feedback.message
+    end
+
+    def user_name
+      @user.name
+    end
+
+    def created_at
+      @feedback.created_on
+    end
+
+  end
+
+end
---
+++ spec/dojo/presenters/feedback_presenter_spec.rb
@@ -0,0 +1,29 @@
+require 'dojo/repository'
+require 'dojo/presenters/feedback_presenter'
+
+describe Dojo::FeedbackPresenter do
+
+  before do
+    repo = Dojo::Repository
+    @user = repo.user.save( repo.user.new( name: "John Doe" ))
+    feedback = { message: "Example Message", user: @user.id }
+    @feedback = repo.feedback.save( repo.feedback.new( feedback ))
+    @presenter = Dojo::FeedbackPresenter.new( @feedback )
+  end
+
+  it "#message returns the Feedback message" do
+    @presenter.message.should be_a( String )
+    @presenter.message.should == "Example Message"
+  end
+
+  it "#user_name returns the Feedback User creator's name" do
+    @presenter.user_name.should be_a( String )
+    @presenter.user_name.should == "John Doe"
+  end
+
+  it "#created_at returns the Feedback creation timestamp" do
+    @presenter.created_at.should be_a( Date )
+    @presenter.created_at.should == @feedback.created_on
+  end
+
+end
